98b48452e6b8710e64112199942864e37ea7120c
Morozov Henry <henrymorozov1@henry-1.ru>
2023-09-16 23:56:35 +0500

    javascripthw

---
+++ homework-one.js
@@ -0,0 +1,31 @@
+
+
+var login = prompt('Введите имя');
+var age = +prompt('Введите возраст');
+var task1 = prompt('Решите пример: 10 + 5 = ')
+
+var task2 = prompt('Решите пример: 20 - 3 = ')
+
+var task3 = prompt('Решите пример: 10 * 3 = ')
+
+var task4 = prompt('Решите пример: 160 / 4 = ')
+
+var task5 = prompt('Решите пример: 13 % 4 = ')
+
+
+console.log('Меня зовут ' + login);
+console.log('Мне ' + age + ' года/лет');
+console.log('Пример 1: 10 + 5 = ' + (10 + 5) + ' Ваш ответ: ' + task1);
+console.log('Пример 2: 20 - 3 = ' + (20 - 3) + ' Ваш ответ: ' + task2);
+console.log('Пример 3: 10 * 3 = ' + 10 * 3 + ' Ваш ответ: ' + task3);
+console.log('Пример 4: 160 / 4 = ' + 160 / 4 + ' Ваш ответ: ' + task4);
+console.log('Пример 5: 13 % 4 = ' + 13 % 4 + ' Ваш ответ: ' + task5);
+
+var X = +prompt('Введите первое число')
+var Y = +prompt('Введите второе число')
+var Z = +prompt('Введите третье число')
+
+var res = (X+Y+Z) / 3
+alert('Среднее арефметическое ' + res)
+
+
